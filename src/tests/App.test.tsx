f2e78c67479e8fd2be95a4799197bb2a2a910c9f
e-mustafa <eng.mustafax@gmail.com>
2025-12-15 15:43:40 +0200

    add unit tests

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,13 +1,14 @@
 import App from '@/App';
 import { render, screen } from '@testing-library/react';
 
+// 1. title should be rendered
 describe('1. App Component', () => {
 	test('1.1 should render title', () => {
 		// Render main app
 		render(<App />);
 
 		// Check UI elements
-		expect(screen.getByTestId('app-title')).toBeInTheDocument();
-
+		const title = screen.getByTestId('app-title');
+		expect(title).toBeInTheDocument();
 	});
 });
